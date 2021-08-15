c990c493a13bb6211d900a2828429dd42ed8be79
mosalah1256 <88688012+mosalah1256@users.noreply.github.com>
2021-08-15 23:51:57 +0200

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c57e57ad3617a178fb81a968e34f960f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e63e2f2a-db50-4f20-84f5-aab3fa7b0b88'
+};
